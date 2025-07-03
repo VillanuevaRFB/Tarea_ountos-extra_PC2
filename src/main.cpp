32f596ac4c9991e70227fa6799fb8b90e9da5720
VillanuevaRFB <rodrigo.villanuevaa@usil.pe>
2025-07-03 09:32:48 -0500

    terminado pero va muy rapido

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,157 @@
-#define F_CPU 16000000L
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-void config_timer(void){
-  TCCR0A|=(1<<WGM01);
-  TCCR0B|=(1<<CS01);
+volatile unsigned int adc_manual=0;
+volatile unsigned int adc_motor=0;
+volatile unsigned char canal=0;
+volatile char c=0;
+
+void config_USART(void){
+  UBRR0=103;
+  UCSR0B|=(1<<TXEN0);
+  UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00);
 }
-void delay_10us(void){
-  OCR0A=20;
-  TCNT0=0;
-  while(!(TIFR0&(1<<OCF0A)));
-  TIFR0|=(1<<OCF0A);
+
+ISR(USART_UDRE_vect){
+  UDR0=c;
+  UCSR0B&=~(1<<UDRIE0);
 }
-unsigned char a=50;
-void pwm(void){
-  DDRB|=0x01;
-  PORTB&=~0x01;
-  config_timer();
-}
-void pwm(unsigned char a){
-  PORTB|=0x01;
-  for(int i=0; i<100+a;i++){
-    delay_10us();
-  }
-  PORTB&=~0x01;
-  for(int i=0;i<1900-a;i++){
-    delay_10us();
+
+void enviar_texto(const char* texto){
+  while(*texto){
+    c=*texto++;
+    UCSR0B|=(1<<UDRIE0);
+    _delay_ms(1);
   }
 }
+
+void enviar_entero(int valor){
+  if(valor>=100){
+    c='0'+(valor/100);
+    UCSR0B|=(1<<UDRIE0);
+    _delay_ms(1);
+  }
+  if(valor>=10){
+    c='0'+((valor/10)%10);
+    UCSR0B|=(1<<UDRIE0);
+    _delay_ms(1);
+  }
+  else{
+    c='0';
+    UCSR0B|=(1<<UDRIE0);
+    _delay_ms(1);
+  }
+  c='0'+(valor%10);
+  UCSR0B|=(1<<UDRIE0);
+  _delay_ms(1);
+}
+//manual=PC0 y motor=PC1
 void config_ADC(void){
-  ADCSRA|=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2);
   ADMUX|=(1<<REFS0);
+  ADCSRA|=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2);
+  canal=0;
+  ADMUX=(ADMUX&0xF0)|0x00;
+  ADCSRA|=(1<<ADSC);
 }
-ISR (ADC_vect){
-  a=ADC*(100.0/1023.0);
+
+ISR(ADC_vect){
+  if(canal==0){
+    adc_manual=ADC;
+    ADMUX=(ADMUX&0xF0)|0x01;
+    canal=1;
+  }
+  else{
+    adc_motor=ADC;
+    ADMUX=(ADMUX&0xF0)|0x00;
+    canal=0;
+  }
+  ADCSRA|=(1<<ADSC);
 }
+//motor en PD2 y PD3
+void config_motor(void){
+  DDRD|=0x0C;
+}
+
+void avanzar(void){
+  PORTD|=0x04;
+  PORTD&=~0x08;
+}
+
+void retroceder(void){
+  PORTD|=0x08;
+  PORTD&=~0x04;
+}
+
+void detener(void){
+  PORTD&=~0x0C;
+}
+
+float adc_a_grados(unsigned int valor_adc){
+  return valor_adc*(270.0/1023.0);
+}
+
+//grados reales (45°-225°) a 0°–180°
+int convercion(float grados_reales){
+  if(grados_reales<45.0||grados_reales>225.0){
+    return-1;
+  }
+  return(int)(grados_reales-45.0);
+}
+
 int main(void){
-  config_timer();
+  config_USART();
   config_ADC();
+  config_motor();
   sei();
+
+  float grados_manual=0;
+  float grados_motor=0;
+  int manual_convertido=0;
+  int motor_convertido=0;
+
   while(1){
-    pwm(a);
-    ADCSRA|=(1<<ADSC);
+    grados_manual=adc_a_grados(adc_manual);
+    grados_motor=adc_a_grados(adc_motor);
+
+    manual_convertido=convercion(grados_manual);
+    motor_convertido=convercion(grados_motor);
+
+    if(grados_motor<45.0||grados_motor>225.0){
+      detener();
+    }
+    else{
+      if(grados_manual>grados_motor+2){
+        avanzar();
+      }
+      else if(grados_manual<grados_motor-2){
+        retroceder();
+      }
+      else{
+        detener();
+      }
+    }
+
+    enviar_texto("Manual:");
+    if(manual_convertido==-1){
+      enviar_texto("Fuera de rango");
+    }
+    else{
+      enviar_entero(manual_convertido);
+      enviar_texto("°");
+    }
+
+    enviar_texto(" | Medido:");
+    if(motor_convertido==-1){
+      enviar_texto("Fuera de rango");
+    }
+    else{
+      enviar_entero(motor_convertido);
+      enviar_texto("°");
+    }
+
+    enviar_texto("\r\n");
+    _delay_ms(1);
   }
 }
